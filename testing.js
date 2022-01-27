85265bee2438827f0b32aa680e90eee2e80592e0
ajaggens <misteryugine@gmail.com>
2022-01-27 21:21:01 +0300

    base js practice

--- testing.js
+++ testing.js
@@ -3,7 +3,6 @@ document.getElementById('test_message').value = 'Ваш код доступа: {
 document.getElementById('selected_networks').value = "25001_25001";
 document.getElementById('partners_pools').value = "615";
 
-application/x-www-form-urlencoded
 
 function selectOption(id, valueToSelect) {    
     let element = document.getElementById(id);
